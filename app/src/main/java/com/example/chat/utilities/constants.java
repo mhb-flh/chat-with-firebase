218023cca1f941ba1f8f7eb3c7ec0c6497475882
mhb <mhb.fallahi@gmail.com>
2021-10-14 19:17:57 +0330

    add keys

--- app/src/main/java/com/example/chat/utilities/constants.java
+++ app/src/main/java/com/example/chat/utilities/constants.java
@@ -1,15 +1,48 @@
 package com.example.chat.utilities;
 
+import java.util.HashMap;
+
 public class constants {
-    public static final String KEY_COLLECTION_USER="users";
-    public static final String KEY_NAME="name";
-    public static final String KEY_EMAIL="email";
-    public static final String KEY_PASSWORD="password";
-    public static final String KEY_PREFERENCE_NAME="chatAppPref";
-    public static final String KEY_IS_SIGN_IN="isSignIn";
-    public static final String KEY_USER_ID="userId";
-    public static final String KEY_IMAGE="image";
-    public static final String KEY_FCM_TOKEN="fcmToken";
+    public static final String KEY_COLLECTION_USER = "users";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_PREFERENCE_NAME = "chatAppPref";
+    public static final String KEY_IS_SIGN_IN = "isSignIn";
+    public static final String KEY_USER_ID = "userId";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_FCM_TOKEN = "fcmToken";
+    public static final String KEY_USER = "user";
+    public static final String KEY_COLLECTION_CHAT = "chat";
+    public static final String KEY_SENDER_ID = "senderId";
+    public static final String KEY_RECEIVER_ID = "receiverId";
+    public static final String KEY_TIMESTAMP = "timestamp";
+    public static final String KEY_MESSAGE = "message";
+    public static final String KEY_COLLECTION_CONVERSATIONS = "conversations";
+    public static final String KEY_SENDER_NAME = "senderName";
+    public static final String KEY_RECEIVER_NAME = "receiverName";
+    public static final String KEY_SENDER_IMAGE = "senderImage";
+    public static final String KEY_RECEIVER_IMAGE = "receiverImage";
+    public static final String KEY_LAST_MESSAGE = "lastMessage";
+    public static final String KEY_AVAILABILITY = "availability";
+    public static final String REMOTE_MSG_AUTHORIZATION = "authorization";
+    public static final String REMOTE_MSG_CONTENT_TYPE = "content-type";
+    public static final String REMOTE_MSG_DATA = "data";
+    public static final String REMOTE_MSG_REGISTRATION_IDS = "registration_ids";
+
+    public static HashMap<String, String> remoteMsgHeaders = null;
+
+    public static HashMap<String, String> getRemoteMsgHeaders() {
+        if (remoteMsgHeaders == null) {
+            remoteMsgHeaders = new HashMap<>();
+            remoteMsgHeaders.put(
+                    REMOTE_MSG_AUTHORIZATION,
+                    "key=AAAAZlAG_-g:APA91bEwVMw28drKI6OalSEc_KBPUHGmXPzgt_ETdE782CngPvYTQHxUHt_qRFfgibR7z_FedYXCKLhc59Xn4UNUdHbyRbvwroZTgLGS18q37BbWzsDXjL48i4pmyDPhP6EGMFGNARCp"
+            );
+            remoteMsgHeaders.put(REMOTE_MSG_CONTENT_TYPE, "application/json");
+        }
+        return remoteMsgHeaders;
+    }
 
 
 }
