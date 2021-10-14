1c219429e08bce6c24246f8028abf01658b44ede
mhb <mhb.fallahi@gmail.com>
2021-10-14 19:16:59 +0330

    chat and user model

---
+++ app/src/main/java/com/example/chat/models/Users.java
@@ -0,0 +1,8 @@
+package com.example.chat.models;
+
+import java.io.Serializable;
+
+public class Users implements Serializable {
+
+    public String name,image,token,email,id;
+}
---
+++ app/src/main/java/com/example/chat/models/chatMessage.java
@@ -0,0 +1,9 @@
+package com.example.chat.models;
+
+import java.util.Date;
+
+public class chatMessage {
+    public String senderId,receiverId,message,DateTime;
+    public Date dateObject;
+    public String conversationId,conversationName,conversationImage;
+}
